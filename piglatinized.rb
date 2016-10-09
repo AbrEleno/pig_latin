814833d154ce03b4cebb72ce4fb1c4c29ecc3b60
Abraham <abraham.eleno@gmail.com>
2016-10-08 20:14:43 -0500

    Versión final refact

--- piglatinized.rb
+++ piglatinized.rb
@@ -1,29 +1,31 @@
 sentence = ARGV
 
-class PigLatin
-@@pig_sentence = []
+#class PigLatin
+# pig_sentence = []
 
   def piglatinized(sentence) 
+    pig_sentence = []
     sentence.each do |word| 
       if word =~ /^[aeiou]/ 
-        @@pig_sentence << word + "way" + " " 
+        pig_sentence << word + "way" + " " 
       elsif word =~ /^[^aeiou]/ 
         n_v = word.slice(/^[^aeiou]+(?=[aeiou])/) 
-        @@pig_sentence << word + n_v + "ay" + " "
+        pig_sentence << word + n_v + "ay" + " "
       else
         p "Can't understand you"
       end
     end
-  @@pig_sentence.each do |wrd| 
+  pig_sentence.each do |wrd| 
       if wrd =~ /^[aeiou]/
        wrd
       else
        wrd.slice!(/^[^aeiou]+(?=[aeiou])/).delete(" ")
       end
     end
-  @@pig_sentence.each {|w| print w}
+  pig_sentence.each {|w| print w}
   end
-end 
+#end 
 
-pig = PigLatin.new
-pig.piglatinized(sentence)   
+piglatinized(sentence)
+# pig = PigLatin.new
+# pig.piglatinized(sentence)   
